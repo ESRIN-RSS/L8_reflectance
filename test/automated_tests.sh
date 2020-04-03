10b79fe1170da188d0d447d120ba91c241a16a2d
Vasco Nunes <vbnunes@criticalsoftware.com>
2020-04-03 14:35:25 +0100

    Update automated_tests.sh

--- test/automated_tests.sh
+++ test/automated_tests.sh
@@ -23,7 +23,7 @@ trap cleanExit EXIT
 function run_l8_reflectance() {
 
   local img=$1
-  L8_reflectance ${img} $( dirname ${img})
+  python L8_reflectance/L8_reflectance.py ${img} $( dirname ${img})
 
   [ ! -d "${img}_TOA" ] && exit 1
 
@@ -38,9 +38,20 @@ function run_l8_reflectance() {
 }
 
 function main() {
-  fels 203031 OLI_TIRS 2015-01-01 2015-06-30 -c 30 -o LANDSAT --latest --outputcatalogs /tmp --overwrite
-  img='LANDSAT/LC08_L1TP_203031_20150518_20170409_01_T1'
-  run_l8_reflectance $img
+  #download sample image:
+  sample_img_url=http://storage.googleapis.com/gcp-public-data-landsat/LC08/PRE/203/031/LC82030312015026LGN00
+  sample_img=LC82030312015026LGN00
+  mkdir ${sample_img}
+  # Declare an array of string with type
+  declare -a StringArray=("B1.TIF", "B2.TIF", "B3.TIF", "B4.TIF", "B5.TIF",
+                          "B6.TIF", "B7.TIF", "B8.TIF", "B9.TIF", "B10.TIF",
+                          "B11.TIF", "BQA.TIF", "MTL.txt" )
+
+  # Iterate the string array using for loop
+  for val in ${StringArray[@]}; do
+     wget ${sample_img_url}/${sample_img}_$val -P ${sample_img}
+  done
+  run_l8_reflectance $sample_img
   [ $? -eq 0 ] || return ${ERR}
 }
 
